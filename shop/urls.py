fe3d0d7e53e123d0b19f553af4d7695f8ae1ded5
Amirhoseinkuhestani <amirhoseinkuhestani@gmail.com>
2023-05-24 20:20:49 +0330

    Notification version

--- shop/urls.py
+++ shop/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import home
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = "shop"
 
 urlpatterns = [
     path('', home, name="home"),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
